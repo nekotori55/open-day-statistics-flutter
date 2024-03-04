2986eaf62f39a487f3784a4897d10a715298bf02
nekotori55 <nekotori55@gmail.com>
2024-03-04 15:04:39 +0300

    Удаление поля "Grade" (оценка) из энтити

--- lib/features/visitors_info/domain/entities/visitor_entity.dart
+++ lib/features/visitors_info/domain/entities/visitor_entity.dart
@@ -4,7 +4,6 @@ import 'school_entity.dart';
 import 'district_entity.dart';
 import 'region_entity.dart';
 
-enum Grade { minor, ninth, tenth, eleventh }
 
 @immutable
 class VisitorEntity extends Equatable {
@@ -14,15 +13,13 @@ class VisitorEntity extends Equatable {
 
   final SchoolEntity school;
 
-  final Grade grade;
 
   const VisitorEntity({
     required this.region,
     required this.district,
     required this.school,
-    required this.grade,
   });
 
   @override
-  List<Object?> get props => [region, district, school, grade];
+  List<Object?> get props => [region, district, school];
 }
