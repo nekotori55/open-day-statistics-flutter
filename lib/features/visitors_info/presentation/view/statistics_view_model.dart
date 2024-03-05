65b7d9b834afad1e13e3eec20dc47708f81ecbcb
nekotori55 <nekotori55@gmail.com>
2024-03-05 20:44:35 +0300

    Добавление StatisticsViewModel

---
+++ lib/features/visitors_info/presentation/view/statistics_view_model.dart
@@ -0,0 +1,38 @@
+import 'package:open_day_statistics_flutter/features/visitors_info/domain/entities/school_entity.dart';
+import 'package:open_day_statistics_flutter/features/visitors_info/domain/entities/statistics_entity.dart';
+import 'package:open_day_statistics_flutter/features/visitors_info/presentation/view/base_view_model.dart';
+import 'package:open_day_statistics_flutter/features/visitors_info/presentation/view/district_view_model.dart';
+import 'package:open_day_statistics_flutter/features/visitors_info/presentation/view/region_view_model.dart';
+import 'package:open_day_statistics_flutter/features/visitors_info/presentation/view/school_view_model.dart';
+
+class StatisticsViewModel<SubjectType extends ViewModel> extends ViewModel {
+
+  final Map<SubjectType, int> subjectToVisitorNumber;
+
+  final int total;
+
+  StatisticsViewModel({required this.subjectToVisitorNumber, required this.total});
+
+
+  factory StatisticsViewModel.fromEntity(StatisticsEntity entity) {
+    late var viewToNum;
+
+    switch (SubjectType) {
+      case RegionViewModel():
+        viewToNum = entity.subjectToVisitorsNumber.map((key, value) => MapEntry(RegionViewModel.fromEntity(key), value));
+      case DistrictViewModel():
+        viewToNum = entity.subjectToVisitorsNumber.map((key, value) => MapEntry(DistrictViewModel.fromEntity(key), value));
+      case SchoolViewModel():
+        viewToNum = entity.subjectToVisitorsNumber.map((key, value) => MapEntry(SchoolViewModel.fromEntity(key), value));
+      default: throw UnimplementedError();
+    }
+
+    return StatisticsViewModel(subjectToVisitorNumber: viewToNum, total:entity.getTotal());
+  }
+
+  @override
+  String toStringByName() {
+    // TODO: implement toStringByName
+    throw UnimplementedError();
+  }
+}
