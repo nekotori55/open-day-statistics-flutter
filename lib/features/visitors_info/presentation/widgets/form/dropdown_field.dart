380224a049813225b5a5aab09c3fe7a80578e80e
nekotori55 <nekotori55@gmail.com>
2024-03-05 13:51:07 +0300

    Добавление анимации выпадающего списка

--- lib/features/visitors_info/presentation/widgets/form/dropdown_field.dart
+++ lib/features/visitors_info/presentation/widgets/form/dropdown_field.dart
@@ -1,7 +1,7 @@
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 
-class DropdownField extends StatelessWidget {
+class DropdownField extends StatefulWidget {
   final List<String> items;
   final String label;
 
@@ -12,36 +12,109 @@ class DropdownField extends StatelessWidget {
   });
 
   @override
+  State<DropdownField> createState() => _DropdownFieldState();
+}
+
+class _DropdownFieldState extends State<DropdownField>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  final _userEditTextController = TextEditingController();
+
+
+  final animationDuration = const Duration(milliseconds: 100);
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: animationDuration);
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return DropdownSearch<String>(
-      items: items,
-      clearButtonProps: ClearButtonProps(isVisible: true),
+      items: widget.items,
+      // clearButtonProps: ClearButtonProps(isVisible: true),
+      onBeforePopupOpening: (_) {
+        return Future(() {
+          _controller.forward();
+          return true;
+        });
+      },
       popupProps: PopupProps.menu(
+      searchFieldProps: TextFieldProps(
+      controller: _userEditTextController,
+      decoration: InputDecoration(
+          suffixIcon: ValueListenableBuilder<TextEditingValue>(
+            valueListenable: _userEditTextController,
+            builder: (context, v, c) => IconButton(
+              icon: const Icon(Icons.clear),
+              enableFeedback: false,
+              // color: Theme.of(context).disabledColor,
+              onPressed: _userEditTextController.value.text.isNotEmpty ? () {
+                _userEditTextController.clear();
+              } : null,
+            ),
+          ),
+      ),
+      ),
+          menuProps: MenuProps(
+            animation: _controller,
+            animationDuration: animationDuration,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+          ),
           showSearchBox: true,
           showSelectedItems: true,
           searchDelay: Duration.zero,
+          onDismissed: () {
+            _controller.reverse();
+          },
+          containerBuilder: (context, child) {
+            return FadeTransition(
+              opacity: _animation,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.vertical(bottom: Radius.circular(20)),
+                  color: Theme.of(context).colorScheme.background,
+                ),
+                child: child,
+              ),
+            );
+          },
           emptyBuilder: (context, searchEntry) {
             return Center(
                 child: Text(
-                  searchEntry.isEmpty ? "Нет данных" : "По запросу ничего не найдено",
-              style: TextStyle(
+              searchEntry.isEmpty
+                  ? "Нет данных"
+                  : "По запросу ничего не найдено",
+              style: const TextStyle(
                 fontSize: 18,
                 color: Colors.grey,
               ),
             ));
           }),
       dropdownDecoratorProps: DropDownDecoratorProps(
-        baseStyle: TextStyle(
+        baseStyle: const TextStyle(
           fontSize: 20,
           fontWeight: FontWeight.w600,
           height: 1.5,
           overflow: TextOverflow.ellipsis,
         ),
         dropdownSearchDecoration: InputDecoration(
-          labelText: label,
+          labelText: widget.label,
           labelStyle: TextStyle(
             color: Theme.of(context).colorScheme.primary,
-            fontSize: 18,
+            fontSize: 18, 
           ),
         ),
       ),
