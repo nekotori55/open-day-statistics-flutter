baa7cb772d1c46a67b54aeba9e6c039165c2385c
nekotori55 <nekotori55@gmail.com>
2024-03-05 14:18:28 +0300

    Рефакторинг dropdown_field.dart

--- lib/features/visitors_info/presentation/widgets/form/dropdown_field.dart
+++ lib/features/visitors_info/presentation/widgets/form/dropdown_field.dart
@@ -17,107 +17,96 @@ class DropdownField extends StatefulWidget {
 
 class _DropdownFieldState extends State<DropdownField>
     with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
+  late AnimationController _animationController;
+  late Animation<double> _animationValue;
 
   final _userEditTextController = TextEditingController();
-
-
-  final animationDuration = const Duration(milliseconds: 100);
+  static const _animationDuration = Duration(milliseconds: 100);
 
   @override
   void initState() {
-    _controller =
-        AnimationController(vsync: this, duration: animationDuration);
-    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+    _animationController = AnimationController(vsync: this, duration: _animationDuration);
+    _animationValue = Tween(begin: 0.0, end: 1.0).animate(_animationController);
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return DropdownSearch<String>(
       items: widget.items,
-      // clearButtonProps: ClearButtonProps(isVisible: true),
-      onBeforePopupOpening: (_) {
-        return Future(() {
-          _controller.forward();
-          return true;
-        });
-      },
+
       popupProps: PopupProps.menu(
-      searchFieldProps: TextFieldProps(
-      controller: _userEditTextController,
-      decoration: InputDecoration(
-          suffixIcon: ValueListenableBuilder<TextEditingValue>(
-            valueListenable: _userEditTextController,
-            builder: (context, v, c) => IconButton(
-              icon: const Icon(Icons.clear),
-              enableFeedback: false,
-              // color: Theme.of(context).disabledColor,
-              onPressed: _userEditTextController.value.text.isNotEmpty ? () {
-                _userEditTextController.clear();
-              } : null,
+          searchFieldProps: TextFieldProps(
+            controller: _userEditTextController,
+            decoration: InputDecoration(
+              suffixIcon: ValueListenableBuilder<TextEditingValue>(
+                valueListenable: _userEditTextController,
+                builder: (context, v, c) => IconButton(
+                  icon: const Icon(Icons.clear),
+                  enableFeedback: false,
+                  // color: Theme.of(context).disabledColor,
+                  onPressed: _userEditTextController.value.text.isNotEmpty
+                      ? () => _userEditTextController.clear()
+                      : null,
+                ),
+              ),
             ),
           ),
-      ),
-      ),
           menuProps: MenuProps(
-            animation: _controller,
-            animationDuration: animationDuration,
+            animation: _animationController,
+            animationDuration: _animationDuration,
             backgroundColor: Colors.transparent,
             shadowColor: Colors.transparent,
           ),
           showSearchBox: true,
           showSelectedItems: true,
           searchDelay: Duration.zero,
-          onDismissed: () {
-            _controller.reverse();
-          },
-          containerBuilder: (context, child) {
-            return FadeTransition(
-              opacity: _animation,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.vertical(bottom: Radius.circular(20)),
-                  color: Theme.of(context).colorScheme.background,
+          onDismissed: () => _animationController.reverse(),
+          containerBuilder: (context, child) => FadeTransition(
+                opacity: _animationValue,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.vertical(
+                        bottom: Radius.circular(20)),
+                    color: Theme.of(context).colorScheme.background,
+                  ),
+                  child: child,
                 ),
-                child: child,
               ),
-            );
-          },
-          emptyBuilder: (context, searchEntry) {
-            return Center(
-                child: Text(
-              searchEntry.isEmpty
-                  ? "Нет данных"
-                  : "По запросу ничего не найдено",
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.grey,
-              ),
-            ));
-          }),
+          emptyBuilder: (context, searchEntry) => Center(
+              child: Text(
+                  searchEntry.isEmpty
+                      ? "Нет данных"
+                      : "По запросу ничего не найдено",
+                  style: const TextStyle(fontSize: 18, color: Colors.grey)))),
       dropdownDecoratorProps: DropDownDecoratorProps(
         baseStyle: const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-          height: 1.5,
-          overflow: TextOverflow.ellipsis,
-        ),
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            height: 1.5,
+            overflow: TextOverflow.ellipsis),
         dropdownSearchDecoration: InputDecoration(
           labelText: widget.label,
           labelStyle: TextStyle(
             color: Theme.of(context).colorScheme.primary,
-            fontSize: 18, 
+            fontSize: 18,
           ),
         ),
       ),
+
+      onBeforePopupOpening: (_) {
+        return Future(() {
+          _animationController.forward();
+          return true;
+        });
+      },
     );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _userEditTextController.dispose();
+    super.dispose();
   }
 }
