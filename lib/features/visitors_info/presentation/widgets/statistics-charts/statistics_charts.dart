75727b3d68bf97bbf9fa3a9833a68101f122563b
Sardelnik <ssa.lolik@gmail.com>
2024-03-12 18:46:03 +0300

    Добавление фона для блока диаграмм

--- lib/features/visitors_info/presentation/widgets/statistics-charts/statistics_charts.dart
+++ lib/features/visitors_info/presentation/widgets/statistics-charts/statistics_charts.dart
@@ -17,26 +17,46 @@ class StatisticsCharts extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 300, child: VisitorsPieChart(data: data)),
-            Container(
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          child: Container(
+            decoration: BoxDecoration(
               color: Colors.white,
-              child: SizedBox(
-                width: 500,
-                height: 300,
-                child: LeadersTable(
-                  border: TableBorder.all(width: 1, color: Colors.grey),
-                  label: label,
-                  data: data.$1.entries
-                      .map((e) => (name: e.key, count: e.value))
-                      .toList(),
-                ),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                )
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 300, child: VisitorsPieChart(data: data)),
+                  Container(
+                    color: Colors.white,
+                    child: SizedBox(
+                      width: 600,
+                      height: 300,
+                      child: LeadersTable(
+                        border: TableBorder.all(width: 1, color: Colors.grey),
+                        label: label,
+                        data: data.$1.entries
+                            .map((e) => (name: e.key, count: e.value))
+                            .toList(),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ],
     );
